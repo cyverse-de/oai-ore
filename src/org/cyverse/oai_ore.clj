9a7262ccc80091d486dc78b74d5fcc588e0bf4c8
Sarah Roberts <sarahr@cyverse.org>
2018-03-31 17:01:36 -0700

    CORE-9176: added some comments and docstrings and renamed a few variables

--- src/org/cyverse/oai_ore.clj
+++ src/org/cyverse/oai_ore.clj
@@ -2,8 +2,10 @@
   (:use [clojure.data.xml :only [alias-uri element]]))
 
 (defprotocol RdfSerializable
-  (to-rdf [_] "Serializes the object as RDF/XML."))
+  (to-rdf [_] "Serializes the object as RDF/XML. This function returns an instance of clojure.data.xml.Element."))
 
+;; These are the namespaces that may be used in the the RDF serialization of the archive. The key is the abbreviation
+;; as it should appear in the serialized XML. The value is the namespace URI.
 (def ^:private namespaces
   {:dc      "http://purl.org/dc/elements/1.1/"
    :dcterms "http://purl.org/dc/terms/"
@@ -13,9 +15,12 @@
    :rdf     "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
    :rdfs    "http://www.w3.org/2000/01/rdf-schema#"})
 
+;; Alias the namespaces so that they can easily be used when generating the XML.
 (apply alias-uri (apply concat namespaces))
 
-(defn- aggregates-element [file-uri]
+(defn- aggregates-element
+  "Gnereates an RDF/XML element indicating that a file is contained within an aggregation."
+  [file-uri]
   (element ::ore/aggregates {::rdf/resource file-uri}))
 
 (deftype Aggregation [uri file-uris avus]
@@ -25,10 +30,10 @@
       (concat [(element ::rdf/type {::rdf/resource "http://www.openarchives.org/ore/terms/Aggregation"})]
               (mapv aggregates-element file-uris)))))
 
-(deftype Archive [archive-uri aggregation-uri]
+(deftype Archive [uri aggregation-uri]
   RdfSerializable
   (to-rdf [_]
-    (element ::rdf/Description {::rdf/about archive-uri}
+    (element ::rdf/Description {::rdf/about uri}
       [(element ::rdf/type {::rdf/resource "http://www.openarchives.org/ore/terms/ResourceMap"})
        (element ::ore/describes {::rdf/resource aggregation-uri})])))
 
@@ -43,7 +48,9 @@
     (element ::rdf/RDF (into {} (map (fn [[k v]] [(keyword "xmlns" (name k)) v]) namespaces))
       (mapv to-rdf descriptions))))
 
-(defn build-ore [aggregation-uri archive-uri file-uris & [avus]]
+(defn build-ore
+  "Generates an ORE archive for a data set."
+  [aggregation-uri archive-uri file-uris & [avus]]
   (Ore. (concat [(Aggregation. aggregation-uri file-uris avus)
                  (Archive. archive-uri aggregation-uri)]
                 (mapv (fn [file-uri] (ArchivedFile. file-uri)) file-uris))))
