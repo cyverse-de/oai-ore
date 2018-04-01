576b4811b54b7e6897aa2851d605b1c16928c3da
Sarah Roberts <sarahr@cyverse.org>
2018-03-31 17:27:17 -0700

    CORE-9176: added a simple unit test

--- test/org/cyverse/oai_ore_test.clj
+++ test/org/cyverse/oai_ore_test.clj
@@ -1,7 +1,17 @@
 (ns org.cyverse.oai-ore-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.data.xml :as xml]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [org.cyverse.oai-ore :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^:private agg-uri "http://foo.org")
+(def ^:private arch-uri "http://foo.org/bar.xml")
+
+;; Comparing generated XML to parsed XML requires us to serialize and parse the generated XML.
+(defn- test-ore [ore filename]
+  (is (= (xml/parse-str (xml/emit-str (to-rdf ore)))
+         (xml/parse (io/reader (io/resource filename))))))
+
+(deftest test-empty-ore
+  (testing "Empty ORE files."
+    (test-ore (build-ore agg-uri arch-uri []) "empty-ore.rdf")))
