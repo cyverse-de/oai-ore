3f0041bbb543a7876a02073a774cf31d3e3db1d1
Sarah Roberts <sarahr@cyverse.org>
2018-04-05 18:52:37 -0700

    bumped the snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/oai-ore "1.0.0"
+(defproject org.cyverse/oai-ore "1.0.1-SNAPSHOT"
   :description "Library for generating OAI-ORE files."
   :url "https://github.com/cyverse-de/oai-ore"
   :license {:name "BSD"
