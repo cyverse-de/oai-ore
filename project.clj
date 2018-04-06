51ad4c62abf2b393e7e759c362853de4382ff51f
Sarah Roberts <sarahr@cyverse.org>
2018-04-05 19:15:52 -0700

    another project.clj fix

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/cyverse-de/oai-ore"
   :license {:name "BSD"
             :url "http://www.cyverse.org/license"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :plugins [[test2junit "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.2.0-alpha5"]]
