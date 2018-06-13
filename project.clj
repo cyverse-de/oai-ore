e8f77a073e48dc92c5b40ff4267dd654cf5d1783
Sarah Roberts <sarahr@cyverse.org>
2018-06-12 18:33:49 -0700

    1.0.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/oai-ore "1.0.2-SNAPSHOT"
+(defproject org.cyverse/oai-ore "1.0.2"
   :description "Library for generating OAI-ORE files."
   :url "https://github.com/cyverse-de/oai-ore"
   :license {:name "BSD"
